6baddffe60062f76c94415d4e6ffc2c027bf2820
yoondgu <doityoungg@gmail.com>
2022-05-17 01:59:03 +0900

    BOJ 2562번 최댓값

---
+++ baekjoon/boj2562Max/Main.java
@@ -0,0 +1,29 @@
+package boj2562Max;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 2562번 최댓값
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] numbers = new int[9];
+		int max = 0;
+		int index = -1;
+		for (int i=0; i<9; i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+			if (i==0 || numbers[i] > max) {	// if문 대신 Math.max 메소드를 사용해볼 수도 있다.
+				max = numbers[i];
+				index = i+1;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(index);
+	}
+}
