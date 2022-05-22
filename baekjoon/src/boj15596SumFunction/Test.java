029d698595cc0b606e9c76b7da6457e58ddca0f0
yoondgu <doityoungg@gmail.com>
2022-05-22 23:38:13 +0900

    BOJ 15596 함수 - 더하기함수

---
+++ baekjoon/src/boj15596SumFunction/Test.java
@@ -0,0 +1,14 @@
+package boj15596SumFunction;
+
+public class Test {
+    long sum(int[] a) {
+        long ans = 0;
+        
+        for (int number:a) {
+            ans += number;
+        }
+        
+        return ans;
+    }
+
+}
